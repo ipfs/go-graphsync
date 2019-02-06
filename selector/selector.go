b361b8ecc9bc3b5911fda9e7459ec9247ecaab3e
hannahhoward <hannah@hannahhoward.net>
2019-02-05 18:47:36 -0800

    docs(diagram): update architecture diagram

--- selector/selector.go
+++ selector/selector.go
@@ -4,22 +4,26 @@ import (
 	ipld "github.com/ipfs/go-ipld-format"
 )
 
+// Selector is an interface for an IPLD Selector.
 type Selector interface {
 	ipld.Node
 }
 
+// SelectionResponse is an interface that represents part of the results
+// of a selector query.
 type SelectionResponse interface {
 	ipld.Node
 }
 
+// SelectionTraverser is an interface for navigating a response to a selector
+// query.
 type SelectionTraverser interface {
 	Next() (SelectionResponse, error)
 	Cancel()
 }
 
-type SelectorManager interface {
+// SelectorQuerier can be used to make and validate selector queries.
+type SelectorQuerier interface {
 	Select(Selector, root ipld.Node) SelectionTraverser
 	Validate(Selector, root ipld.Node, incomingResponses SelectionTraverser) SelectionTraverser
-	DecodeSelector([]byte) Selector
-	DecodeSelectorResponse([]byte) SelectionResponse
 }
