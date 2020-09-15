4c9b3c863031bfb211a5b83d62283b7756e3e092
Aarsh Shah <aarshkshah1992@gmail.com>
2020-09-15 17:58:43 +0530

    solve race in context cancellation

--- requestmanager/responsecollector.go
+++ requestmanager/responsecollector.go
@@ -88,6 +88,16 @@ func (rc *responseCollector) collectResponses(
 			case err, ok := <-incomingErrors:
 				if !ok {
 					incomingErrors = nil
+					// even if the `incomingErrors` channel is closed without any error,
+					// the context could still have timed out in which case we need to inform the caller of the same.
+					select {
+					case <-requestCtx.Done():
+						select {
+						case <-rc.ctx.Done():
+						case returnedErrors <- graphsync.RequestContextCancelledErr{}:
+						}
+					default:
+					}
 				} else {
 					receivedErrors = append(receivedErrors, err)
 				}
