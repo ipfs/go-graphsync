a37371782179c3557253499b37e68dcdd558ceaa
Hannah Howard <hannah@hannahhoward.net>
2020-12-21 20:15:00 -0800

    Update graphsync.go
    Co-authored-by: dirkmc <dirkmdev@gmail.com>

--- graphsync.go
+++ graphsync.go
@@ -289,7 +289,7 @@ type OnBlockSentListener func(p peer.ID, request RequestData, block BlockData)
 // OnNetworkErrorListener runs when queued data is not able to be sent
 type OnNetworkErrorListener func(p peer.ID, request RequestData, err error)
 
-// OnNetworkErrorListener runs when queued data is not able to be received
+// OnReceiverNetworkErrorListener runs when errors occur receiving data over the wire
 type OnReceiverNetworkErrorListener func(p peer.ID, err error)
 
 // OnResponseCompletedListener provides a way to listen for when responder has finished serving a response
