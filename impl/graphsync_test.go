35d5ada279af0a0741349fc9753c7e664a7e274a
hannahhoward <hannah@hannahhoward.net>
2021-12-14 22:54:40 -0800

    test(impl): add test to verify single abort message

--- impl/graphsync_test.go
+++ impl/graphsync_test.go
@@ -980,6 +980,8 @@ func TestNetworkDisconnect(t *testing.T) {
 	tracing := collectTracing(t)
 	traceStrings := tracing.TracesToStrings()
 	require.Contains(t, traceStrings, "response(0)->executeTask(0)")
+	require.Contains(t, traceStrings, "response(0)->abortRequest(0)")
+	require.NotContains(t, traceStrings, "response(0)->abortRequest(1)")
 	// may contain multiple abortRequest traces as the network error can bubble up >1 times
 	// but these will only record if the request is still executing
 	require.Contains(t, traceStrings, "request(0)->newRequest(0)")
