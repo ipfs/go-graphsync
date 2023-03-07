7aa9006dbfe64bbf36dcd68c2461c720d6a1b5b5
Jacob Heun <jacob.heun@gmail.com>
2023-03-07 20:13:28 +0100

    fix: use the more targeted shutdown

--- messagequeue/messagequeue.go
+++ messagequeue/messagequeue.go
@@ -245,7 +245,13 @@ func (mq *MessageQueue) sendMessage() {
 		log.Infof("cant open message sender to peer %s: %s", mq.p, err)
 		// TODO: cant connect, what now?
 		mq.publishError(metadata, fmt.Errorf("cant open message sender to peer %s: %w", mq.p, err))
-		return
+		select {
+		case <-mq.done:
+			return
+		default:
+			mq.Shutdown()
+			return
+		}
 	}
 
 	for i := 0; i < mq.maxRetries; i++ { // try to send this message until we fail.
@@ -377,12 +383,4 @@ func (mq *MessageQueue) publishError(metadata internalMetadata, err error) {
 	mq.scrubResponseStreams(metadata.responseStreams)
 	mq.eventPublisher.Publish(metadata.topic, Event{Name: Error, Err: err, Metadata: metadata.public})
 	_ = mq.allocator.ReleaseBlockMemory(mq.p, metadata.msgSize)
-
-	select {
-	case <-mq.done:
-		return
-	default:
-		mq.Shutdown()
-		return
-	}
 }
